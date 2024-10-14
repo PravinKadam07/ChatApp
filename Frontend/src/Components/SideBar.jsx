40211dc02e1ec47c709e8b43903770c749000d63
Pravin Kadam <pravin.kadam@jcilpune.com>
2024-10-14 13:06:32 +0530

    Project setup (basic stracture

---
+++ Frontend/src/Components/SideBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./myStyle.css"
+function SideBar() {
+  return (
+    <div className='side-bar'>SideBar</div>
+  )
+}
+
+export default SideBar
---
+++ Frontend/src/Components/WorkArea.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./myStyle.css"
+function WorkArea() {
+  return (
+    <div className='work-area'>WorkArea</div>
+  )
+}
+
+export default WorkArea
